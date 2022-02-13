12e5c793ff817dcd718c231e687e8f0bf5eae0a0
Gentle Possel <639567@student.inholland.nl>
2022-02-13 14:12:21 +0100

    Add project files.

---
+++ Recursion_Math_Lesson_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursion_Math_Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
