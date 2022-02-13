bf417f7f33940df4e4d6da28358828b5f674cffc
Gentle Possel <639567@student.inholland.nl>
2022-02-13 14:12:36 +0100

    First setup

--- Recursion_Math_Lesson_1/Program.cs
+++ Recursion_Math_Lesson_1/Program.cs
@@ -7,6 +7,7 @@ namespace Recursion_Math_Lesson_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
